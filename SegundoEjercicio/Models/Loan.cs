791e9068763215cb84c7b49d2dbe3565ae381720
pompone <54503871+pompone@users.noreply.github.com>
2025-10-30 19:30:41 -0300

    Update Loan.cs

--- SegundoEjercicio/Models/Loan.cs
+++ SegundoEjercicio/Models/Loan.cs
@@ -1,17 +1,34 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace SegundoEjercicio.Models
 {
     public class Loan
     {
         public int Id { get; set; }
+
+        [Display(Name = "Libro")]
         public int BookId { get; set; }
         public Book? Book { get; set; }
+
+        [Display(Name = "Socio")]
         public int MemberId { get; set; }
         public Member? Member { get; set; }
 
-        [DataType(DataType.Date)] public DateTime LoanDate { get; set; } = DateTime.Today;
-        [DataType(DataType.Date)] public DateTime DueDate { get; set; } = DateTime.Today.AddDays(14);
-        [DataType(DataType.Date)] public DateTime? ReturnDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de préstamo")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime LoanDate { get; set; } = DateTime.Today;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de devolución")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DueDate { get; set; } = DateTime.Today.AddDays(14);
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de devolución")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? ReturnDate { get; set; }
     }
 }
