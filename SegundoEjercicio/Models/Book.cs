99d3e84532dbff76c6defa8fddf97196c991c292
pompone <54503871+pompone@users.noreply.github.com>
2025-10-31 20:25:38 -0300

    Update Book.cs

--- SegundoEjercicio/Models/Book.cs
+++ SegundoEjercicio/Models/Book.cs
@@ -2,31 +2,37 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SegundoEjercicio.Models
 {
-    public class Loan
+    public class Book
     {
         public int Id { get; set; }
 
-        [Required]
-        [Display(Name = "Libro")]
-        public int BookId { get; set; }
-        public Book? Book { get; set; }
+        [Required, StringLength(180)]
+        [Display(Name = "Título")]
+        public string Title { get; set; } = "";
 
-        [Required]
-        [Display(Name = "Socio")]
-        public int MemberId { get; set; }
-        public Member? Member { get; set; }
+        [Display(Name = "Autor")]
+        public int AuthorId { get; set; }
+        public Author? Author { get; set; }
 
-        [Display(Name = "Fecha de préstamo")]
-        [DataType(DataType.Date)]
-        public DateTime LoanDate { get; set; } = DateTime.Today;
+        [Display(Name = "Categoría")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
 
-        [Display(Name = "Fecha de vencimiento")]
-        [DataType(DataType.Date)]
-        public DateTime DueDate { get; set; } = DateTime.Today.AddDays(14);
+        [Display(Name = "Año de publicación")]
+        [Range(1000, 2100, ErrorMessage = "Ingrese un año válido.")]
+        public int? PublishedYear { get; set; }
 
-        [Display(Name = "Fecha de devolución")]
-        [DataType(DataType.Date)]
-        public DateTime? ReturnDate { get; set; }
+        [StringLength(32)]
+        [Display(Name = "ISBN")]
+        public string? ISBN { get; set; }
+
+        [Display(Name = "Ejemplares disponibles")]
+        [Range(0, int.MaxValue, ErrorMessage = "No puede ser negativo.")]
+        public int CopiesAvailable { get; set; } = 1;
+
+        [Display(Name = "Préstamos")]
+        public ICollection<Loan> Loans { get; set; } = new List<Loan>();
     }
 }
+
 
