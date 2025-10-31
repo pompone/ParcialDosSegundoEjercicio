82a59d399ee3beb68b07b5f1e9c0ed3d215c16de
pompone <54503871+pompone@users.noreply.github.com>
2025-10-31 20:20:59 -0300

    Update Book.cs

--- SegundoEjercicio/Models/Book.cs
+++ SegundoEjercicio/Models/Book.cs
@@ -1,18 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace SegundoEjercicio.Models
 {
-    public class Book
+    public class Loan
     {
         public int Id { get; set; }
-        [Required, StringLength(180)] public string Title { get; set; } = "";
-        public int AuthorId { get; set; }
-        public Author? Author { get; set; }
-        public int CategoryId { get; set; }
-        public Category? Category { get; set; }
-        public int? PublishedYear { get; set; }
-        [StringLength(32)] public string? ISBN { get; set; }
-        public int CopiesAvailable { get; set; } = 1;
-        public ICollection<Loan> Loans { get; set; } = new List<Loan>();
+
+        [Required]
+        [Display(Name = "Libro")]
+        public int BookId { get; set; }
+        public Book? Book { get; set; }
+
+        [Required]
+        [Display(Name = "Socio")]
+        public int MemberId { get; set; }
+        public Member? Member { get; set; }
+
+        [Display(Name = "Fecha de préstamo")]
+        [DataType(DataType.Date)]
+        public DateTime LoanDate { get; set; } = DateTime.Today;
+
+        [Display(Name = "Fecha de vencimiento")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; } = DateTime.Today.AddDays(14);
+
+        [Display(Name = "Fecha de devolución")]
+        [DataType(DataType.Date)]
+        public DateTime? ReturnDate { get; set; }
     }
 }
+
